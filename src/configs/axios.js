1e2440843a099ae459c59829f20d70b2a323cba6
nsbbezerra <nsbbezerra@hotmail.com>
2020-07-15 13:34:40 -0300

    adjust port of application

--- src/configs/axios.js
+++ src/configs/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 var url = "https://palmieri-api.herokuapp.com";
 var local = "http://localhost:21108";
-var production = "http://palmieriuniformes.com:21108";
+var production = "http://palmieriuniformes.com:21489";
 
 const api = axios.create({
   baseURL: production,
